5c5fafb0307a3ec327870125a37f43be30c82a66
Md Asad Azam <86871656+md-asad-azam@users.noreply.github.com>
2021-12-18 19:57:29 +0530

    HackerRank

---
+++ Circular Array Rotation.java
@@ -0,0 +1,99 @@
+Circular Array Rotation | HackerRank
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'circularArrayRotation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+    // Write your code here
+    // int i, n, p;
+    List<Integer> arr = new ArrayList<>();
+        // while(k-- != 0){
+        //     p = a.get(0);
+        //     for(i = 0; i < a.size(); ++i){
+        //         n = a.get((i+1) % a.size());
+        //         a.set((i+1) % a.size(), p);
+        //         p = n;
+        //     }
+        // }
+        // for(i = 0; i < queries.size(); ++i){
+        //     arr.add(a.get(queries.get(i)));
+        // }
+        // return arr;
+        
+        int pos, n = a.size();
+        for(int i = 0; i < queries.size(); ++i){
+            pos = queries.get(i) - k;
+            
+            while(pos < 0)  pos += n;
+            
+            pos %= n;
+            
+            arr.add(a.get(pos));
+        }
+        return arr;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.circularArrayRotation(a, k, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
